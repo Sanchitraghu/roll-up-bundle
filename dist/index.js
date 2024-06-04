d69146b88bbe9342dd6b3dd2811c4e5bf8f97f43
sanchit <sanchit.raghuwanshi@rapidops.co>
2024-06-04 11:41:26 +0530

    roll-up-bundle

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+export { default as sum } from './sum.js';
+export { default as subtract } from './subtract.js';
+export { default as multiply } from './multiply.js';
+export { default as divide } from './divide.js';
